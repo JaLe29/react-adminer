db106a34630c6e8f704ba4d3524c081dd2bc164d
mastnyt <mastnyt.00@spst.eu>
2022-09-22 22:03:49 +0200

    Moznost odstaranit obl. filter

--- packages/react-adminer/src/components/FavoriteList.tsx
+++ packages/react-adminer/src/components/FavoriteList.tsx
@@ -1,6 +1,8 @@
-import { Alert, Table as TableAntd } from 'antd';
-import { useEffect, useState } from 'react';
+import { DeleteOutlined } from '@ant-design/icons';
+import { Alert, Popconfirm, Table as TableAntd } from 'antd';
+import { useEffect, useReducer, useState } from 'react';
 import { useReactAdminerContext } from '../hooks/useReactAdminerContext';
+import LineSpaceBetween from './LineSpaceBetween';
 
 interface Props {
 	entityName?: string;
@@ -13,28 +15,26 @@ const FavoriteList: React.FC<Props> = inputEntityName => {
 	const [localStorageData, setLocalStorageData] = useState<undefined | any>(undefined);
 	const [isEmpty, setIsEmpty] = useState<undefined | boolean>();
 	const { entityName } = inputEntityName;
-	const finalObjContent = { favourites: [{}] };
+	const [reducerValue, forceUpdate] = useReducer(x => x + 1, 0);
 
 	useEffect((): void => {
 		const actualLocalStorage = localStorage.getItem('react-adminer');
 		if (actualLocalStorage) {
 			try {
+				const finalObjContent: { favourites: { name: string; entity: string; payload: string }[] } = {
+					favourites: [],
+				};
 				const storageObj = JSON.parse(actualLocalStorage);
-				if (!entityName) {
-					console.log('neco');
-					setLocalStorageData(storageObj);
-					return;
-				}
-				storageObj.favourites.foreach((e: any): void => {
-					if (e.entity === entityName) {
+				storageObj.favourites.forEach((e: any): void => {
+					if (e.entity === entityName || !entityName) {
 						finalObjContent.favourites.push({ name: e.name, entity: e.entity, payload: e.payload });
 					}
 				});
-				if (Object.keys(finalObjContent.favourites).length > 1) {
-					finalObjContent.favourites.shift();
+				if (Object.keys(finalObjContent.favourites).length > 0) {
 					setLocalStorageData(finalObjContent);
 				} else {
 					setIsEmpty(true);
+					setLocalStorageData(undefined);
 				}
 				return;
 			} catch {
@@ -42,7 +42,14 @@ const FavoriteList: React.FC<Props> = inputEntityName => {
 			}
 		}
 		setIsEmpty(true);
-	}, []);
+	}, [reducerValue]);
+
+	const deleteFavoriteFilter = (record: any): void => {
+		const favourites = localStorageData.favourites.filter((i: any) => i.name !== record.name);
+		localStorage.setItem('react-adminer', JSON.stringify({ favourites }));
+		setLocalStorageData(favourites);
+		forceUpdate();
+	};
 
 	const columns = [
 		{
@@ -50,14 +57,26 @@ const FavoriteList: React.FC<Props> = inputEntityName => {
 			dataIndex: 'name',
 			key: 'name',
 			render: (text: any, record: any) => (
-				<div
-					style={{ cursor: 'pointer', color: '#1890FF' }}
-					onClick={() => {
-						navigate(`${paths?.listPath ?? ''}/${record.entity}?f=${JSON.stringify(record.payload)}`);
-					}}
-				>
-					{text}
-				</div>
+				<LineSpaceBetween>
+					<div
+						style={{ cursor: 'pointer', color: '#1890FF' }}
+						onClick={() => {
+							navigate(`${paths?.listPath ?? ''}/${record.entity}?f=${JSON.stringify(record.payload)}`);
+						}}
+					>
+						{text}
+					</div>
+					<div>
+						<Popconfirm
+							title="Really want to remove this favorite filter?"
+							onConfirm={() => deleteFavoriteFilter(record)}
+							okText="Yes"
+							cancelText="No"
+						>
+							<DeleteOutlined />
+						</Popconfirm>
+					</div>
+				</LineSpaceBetween>
 			),
 		},
 		{
@@ -66,7 +85,7 @@ const FavoriteList: React.FC<Props> = inputEntityName => {
 			key: 'entity',
 		},
 	];
-	console.log(localStorageData);
+
 	return (
 		<>
 			{localStorageData && <TableAntd dataSource={localStorageData.favourites} columns={columns} />}
