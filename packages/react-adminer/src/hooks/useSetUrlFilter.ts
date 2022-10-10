aab7558eb452084715a3fb8756e9cb5c55123b6e
Jakub Löffelmann <jakub.loffelmann@livesport.eu>
2022-10-10 16:33:06 +0200

    SQ

--- packages/react-adminer/src/hooks/useSetUrlFilter.ts
+++ packages/react-adminer/src/hooks/useSetUrlFilter.ts
@@ -1,18 +1,7 @@
+import { jsonDeserializeParse, jsonSerializeParse } from '../utils/config';
 import useStateParams from './useStateParams';
 
 export default function useSetUrlFilter(): any {
-	const [filterConfig, setFilter] = useStateParams<any>(
-		undefined,
-		'f',
-		(v?: string) => {
-			try {
-				const parsed = JSON.parse(v ?? 'empty');
-				return parsed;
-			} catch {
-				return {};
-			}
-		},
-		(v: string) => JSON.stringify(v),
-	);
+	const [filterConfig, setFilter] = useStateParams<any>(undefined, 'f', jsonDeserializeParse, jsonSerializeParse);
 	return [filterConfig, setFilter];
 }
